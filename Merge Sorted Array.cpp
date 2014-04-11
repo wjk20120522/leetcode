2869f04aa86df6d7808358b0dadaef1e66a7841d
wjk <jkwangbest@gmail.com>
2014-04-11 13:30:36 +0800

    add a comment

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(int A[], int m, int B[], int n) {
+    	// use STL function
     	copy( B, B + n, A + m);
 		sort(A, A + m + n);
 		/** another concrete method
