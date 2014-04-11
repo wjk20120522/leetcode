24357efdc039c17a501423c769b23f3b52040325
wjk <jkwangbest@gmail.com>
2014-04-11 13:16:22 +0800

    add another concrete method

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -3,9 +3,23 @@ public:
     void merge(int A[], int m, int B[], int n) {
     	copy( B, B + n, A + m);
 		sort(A, A + m + n);
-        /*int i,j;
-        for(i=0,j=0; i<m,j<n;) {
-            if(A[i])
-        }*/
+		/** another concrete method
+		 *********
+        //put the num from the tail of array A
+        int index = m+n-1;
+        for(; index>=0; index--) {
+        	if(n<=0) {
+        		return ;
+        	} else if(m<=0) {
+        		A[index] = B[--n];
+        	} else if(A[m-1] < B[n-1]) {
+        		A[index] = B[--n];
+        	} else {
+        		A[index] = A[--m];
+        	}
+
+        }
+        ***********
+        **/
     }
 };
