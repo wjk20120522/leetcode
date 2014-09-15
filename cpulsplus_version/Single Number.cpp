2eb693f8f2950b09787b8c4479b44e7dfe3a269b
jkwang <jkwangbest@gmail.com>
2014-09-15 16:31:05 +0800

    2014/09/15 first problem

--- cpulsplus_version/Single Number.cpp
+++ cpulsplus_version/Single Number.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
-    int singleNumber(int A[], int n) {
+    int singleNumber(int A[], int n) 
+    {
         n--;
-		for(; n>=1; n--) {
-        	A[n-1] = A[n]^A[n-1];  
+		for(; n>=1; n--)
+        {
+        	A[n-1] ^= A[n];
         }
         return A[0];
     }
 };
 
-int main() {
+int main() 
+{
 	Solution s;
 	int Arr[] = {1,1,2,3,2,3,4,5,5};
 	cout<<s.singleNumber(Arr,9)<<endl;
