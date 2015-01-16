0b8ad0769e614eab11085547097dea4f94b73a94
wjk20120522 <jkwangbest@gmail.com>
2015-01-16 16:20:22 +0800

    inorder  beautiful code

---
+++ cpulsplus_version/Binary Search Tree Iterator.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+	BSTIterator(TreeNode *root) {
+		
+		while (!stack.empty() || root != NULL) {
+			if (root) {
+				stack.push_back(root);
+				root = root->left;
+			}
+			else {
+				root = stack.back();
+				res.push_back(root->val);
+				stack.pop_back();
+				
+				root = root->right;
+			
+			}	
+		}
+
+		pos = 0;
+	}
+
+	/** @return whether we have a next smallest number */
+	bool hasNext() {
+		return pos < res.size();
+	//	return !s.empty();
+	}
+
+	/** @return the next smallest number */
+	int next() {
+		return res[pos++];
+	}
+private:
+	vector<int> res;
+	vector<TreeNode *> stack;
+	int pos;
+};
