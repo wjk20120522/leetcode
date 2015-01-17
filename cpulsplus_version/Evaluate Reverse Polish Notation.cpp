7d52b4d82a71f7a5ca9b57f7e93bee9e2dda48b8
wjk20120522 <jkwangbest@gmail.com>
2015-01-17 10:54:08 +0800

    smarter be careful

--- cpulsplus_version/Evaluate Reverse Polish Notation.cpp
+++ cpulsplus_version/Evaluate Reverse Polish Notation.cpp
@@ -48,3 +48,44 @@ public:
     	}
     }
 };
+
+/**
+ * another time doing it
+ */
+/*
+
+class Solution {
+public:
+    int evalRPN(vector<string> &tokens) {
+        stack<int> s;
+          int res = 0;
+          string tmp;
+          for (int i = 0; i < tokens.size(); i++) {
+              tmp = tokens[i];
+              if (!isdigit(tmp[0]) && tmp.size() == 1) { // operators, be careful about -4 and -
+                  int one = s.top(); s.pop();
+                  int two = s.top(); s.pop();
+                  switch (tmp[0])
+                  {
+                  case '+':
+                      s.push(two + one); break;
+                  case '-':
+                      s.push(two - one); break;
+                  case '/':
+                      s.push(two / one); break;
+                  case '*':
+                      s.push(two * one); break;
+                  default:
+                      break;
+                  }
+
+              }
+              else {
+                  s.push(stoi(tmp));            //operand
+              }
+          }
+          return s.top();
+    }
+};
+
+ */
