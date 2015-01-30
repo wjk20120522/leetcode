ba334b4be0ab9f74ef02a22f36fefe8488caf719
wjk20120522 <jkwangbest@gmail.com>
2015-01-30 14:11:29 +0800

    recursive method

---
+++ cplusplus_version/Regular Expression Matching.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if (!*p) return !*s;       // p is empty
+
+		// p is not empty
+		if (*(p + 1) == '*') {
+			return (isMatch(s, p + 2) || *s && (*s == *p || *p == '.') && isMatch(s + 1, p));
+		}
+		else {
+			if (!*s) return false;
+			return (*s == *p || *p == '.') ?  isMatch(s + 1, p + 1) : false ;
+
+		}
+    }
+};
