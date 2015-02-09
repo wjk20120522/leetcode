d01358037dc533edb5bea06e014d2d6aea128c80
wjk20120522 <jkwangbest@gmail.com>
2015-02-09 19:37:07 +0800

    easy to understand method

---
+++ cplusplus_version/Longest Valid Parentheses.cpp
@@ -0,0 +1,37 @@
+/**
+ * easy to understand
+ */
+class Solution {
+public:
+	int longestValidParentheses(string s) {
+		//int v[]
+		int len = s.length();
+		if (len == 0) return 0;
+		vector<int> v(len);
+		int max = 0;
+		/**
+		 * s[i] = (  => 0
+		 * s[i] = )   s[i-1] = (  => 2 + v[i-2]
+		 * 			  s[i-1] = )  =>  2 + v[i-1]  + v[i-2-v[i-1]]
+		 */
+		for (int i = 0; i < len; i++) {
+			if (s[i] == '(') { v[i] = 0; }
+			else {     // ')'
+				if (i>0 && s[i - 1] == '(') {
+					v[i] = 2 + (i >= 2 ? v[i - 2] : 0 );
+				}
+				else if (i > 0 && s[i - 1] == ')') {
+					if (  i-1-v[i-1] >=0 && s[i - 1 - v[i - 1]] == '(') {
+						v[i] = v[i - 1] + 2 + ( (i - 1 - v[i - 1] > 0) ? v[i - 2 - v[i - 1]] : 0 );
+					}
+				}
+				else {
+					v[i] = 0;
+				}
+				max = max > v[i] ? max : v[i];
+			}
+		}
+		return max;
+
+	}
+};
