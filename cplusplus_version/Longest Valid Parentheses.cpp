fd52b1df544ba8db0f2ae13d3678fd821b330ca6
wjk20120522 <jkwangbest@gmail.com>
2015-02-09 19:45:02 +0800

    more clean code

--- cplusplus_version/Longest Valid Parentheses.cpp
+++ cplusplus_version/Longest Valid Parentheses.cpp
@@ -35,3 +35,24 @@ public:
 
 	}
 };
+
+/**
+ * more clean and conscise code
+ */
+/*
+class Solution {
+public:
+	int longestValidParentheses(string s) {
+	    int len = s.length();
+		vector<int> v(len);
+		int max = 0;
+		for (int i = 1; i < len; i++) {
+			if (s[i] == ')' &&  i - 1 - v[i - 1] >= 0 && s[i - 1 - v[i - 1]] == '(') {    // if the match parenthese
+				v[i] = v[i - 1] + 2 + ((i - 1 - v[i - 1] > 0) ? v[i - 2 - v[i - 1]] : 0);
+			}
+			max = max > v[i] ? max : v[i];
+		}
+		return max;
+	}
+};
+*/
