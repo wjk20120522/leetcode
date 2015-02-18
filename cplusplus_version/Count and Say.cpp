190b8b67f7f085cb294fff4f141e840e5e58a33e
wjk20120522 <jkwangbest@gmail.com>
2015-02-18 18:03:58 +0800

    more short code

--- cplusplus_version/Count and Say.cpp
+++ cplusplus_version/Count and Say.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        string res = "1";
-		string tmp;
-		int count, len;
-		for (int i = 2; i <= n; i++)
-		{
+	string countAndSay(int n) {
+		string res = "1", tmp;
+		for (int i = 1; i < n; i++) {
 			tmp = "";
-			len = res.size();
-			for (int j = 0; j<len; j++)
-			{
-				if (j == len - 1)
-				{
-					tmp += "1";
-					tmp += res[len - 1];
-					//tmp += "1" + res[len - 1];
-					break;
-				}
-				count = 1;
-				while (res[j] == res[j + 1] && j<len - 1)
-				{
-					j++;
-					count++;
-				}
-				stringstream ss;
-				ss << count;
-				tmp += ss.str();
-				tmp += res[j];
-
+			int len = res.length();
+			for (int j = 0; j < len; j++) {
+				int begin = j;
+				while (j < len - 1 && res[j] == res[j + 1]) j++;
+				tmp += to_string(j - begin+1) + res[begin];
 			}
-
 			res = tmp;
 		}
 		return res;
-    }
+	}
 };
