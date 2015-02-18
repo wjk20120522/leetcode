a0542ecee74778a3d2153d5fcb1177cb4ba0873c
wjk20120522 <jkwangbest@gmail.com>
2015-02-18 19:03:27 +0800

    another short but not clean code

--- cplusplus_version/Excel Sheet Column Title.cpp
+++ cplusplus_version/Excel Sheet Column Title.cpp
@@ -14,3 +14,16 @@ public:
 		}
     }
 };
+/* shorter...
+string convertToTitle(int n) {
+	string res;
+	while (n) {	
+		res = char('A' + (n-1)%26) + res;
+		n = (n-1)/26;
+	}
+	return res;
+}
+*/
+		
+
+
