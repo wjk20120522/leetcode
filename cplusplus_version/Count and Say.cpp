566c680e00bcf557ae6ca7b307ef075a155fb8fc
wjk20120522 <jkwangbest@gmail.com>
2015-02-18 18:38:01 +0800

    more clean code

--- cplusplus_version/Count and Say.cpp
+++ cplusplus_version/Count and Say.cpp
@@ -7,6 +7,7 @@ public:
 			int len = res.length();
 			for (int j = 0; j < len; j++) {
 				int begin = j;
+				// j-begin+1 : the number of res[begin]
 				while (j < len - 1 && res[j] == res[j + 1]) j++;
 				tmp += to_string(j - begin+1) + res[begin];
 			}
--- cplusplus_version/Excel Sheet Column Number.cpp
+++ cplusplus_version/Excel Sheet Column Number.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int titleToNumber(string s) {
         int res = 0;
-		int val;
 		for (char c : s) {
-			val = c - 'A' + 1;
-			res = res * 26 + val;
+			res = res * 26 + c - 'A' + 1;
 		}
 		return res;
+
         
     }
 };
