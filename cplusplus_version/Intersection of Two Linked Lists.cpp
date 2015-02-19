529c57adece3cbbfec17a46e8d1789d286e6a8ef
wjk20120522 <jkwangbest@gmail.com>
2015-02-19 11:21:59 +0800

    fundamental problem

--- cplusplus_version/Intersection of Two Linked Lists.cpp
+++ cplusplus_version/Intersection of Two Linked Lists.cpp
@@ -6,37 +6,36 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/*
+calculate the length of two linklist and make the longer linklist pointer go some steps
+ */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA == NULL || headB == NULL) return NULL;
-        int len1 = getLength(headA);
-        int len2 = getLength(headB);
-        ListNode *ptrA = headA, *ptrB = headB;
-        int dis = len1-len2;
-        if(dis>0) {
-            for(int i=0; i<dis; i++) {
-                ptrA = ptrA->next;
-            }
-        } else {
-            for(int i=0; i<-dis; i++) {
-                ptrB = ptrB->next;
+        if (headA == NULL || headB == NULL) return NULL;
+        int len1 = getLength(headA), len2 = getLength(headB);
+        if (len1 > len2) {
+            for (int i = 0; i < len1 - len2; i++) {
+                headA = headA->next;
             }
         }
-        while( ptrA != NULL && ptrA != ptrB ) {
-            ptrA = ptrA->next;
-            ptrB = ptrB->next;
+        else if (len1 < len2) {
+            for (int i = 0; i < len2 - len1; i++) {
+                headB = headB->next;
+            }
         }
-        return ptrA;
+        while (headA != headB) {
+            headB = headB->next;
+            headA = headA->next;
+        }
+        return headA;
     }
-    
-    int getLength(ListNode *head) {
-        if(head == NULL) return 0;
-        int count = 0;
-        while(head) {
-            count ++;
-            head = head->next;
+
+    int getLength(ListNode *root) {
+        int res = 0;
+        while (root) {
+            res++; root = root->next;
         }
-        return count;
+        return res;
     }
 };
