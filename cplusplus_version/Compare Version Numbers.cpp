ea9f89bfce4ad2ae1e0f44b7b34fb81c0118934b
wjk20120522 <jkwangbest@gmail.com>
2015-02-19 10:57:14 +0800

    two methods now

--- cplusplus_version/Compare Version Numbers.cpp
+++ cplusplus_version/Compare Version Numbers.cpp
@@ -15,3 +15,29 @@ public:
 		  return 0;
     }
 };
+
+
+/*
+same idea, but use int to save the two tmp chilid version
+ */
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int len1 = version1.length(), len2 = version2.length();
+		int count1 = 0, count2 = 0;
+		for (int i = 0, j=0; i < len1 || j < len2; i++,j++) {
+			while (i < len1 && version1[i] != '.') {
+				count1 = count1 * 10 + version1[i] - '0';
+				i++;
+			}
+			while (j < len2 && version2[j] != '.') {
+				count2 = count2 * 10 + version2[j] - '0';
+				j++;
+			}
+			if (count1 > count2) return 1;
+			if (count1 < count2) return -1;
+			count1 = 0, count2 = 0;
+		}
+		return 0;
+    }
+};
