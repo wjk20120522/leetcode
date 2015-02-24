5b254a9005b7479f9cb763807e160a65715efea9
wjk20120522 <jkwangbest@gmail.com>
2015-02-24 16:46:09 +0800

    code shorter

--- cplusplus_version/Add Binary.cpp
+++ cplusplus_version/Add Binary.cpp
@@ -1,77 +1,19 @@
-string res = "";
-		int len1 = a.length();
-		int len2 = b.length();
-
-		int end1 = len1 - 1;
-		int end2 = len2 - 1;
-		
-		int tmp;
-		int digit = 0;
-		while (end1 >= 0 && end2 >= 0)
-		{
-			tmp = a[end1] - '0' + b[end2] - '0' + digit;
-			if (tmp >= 2)
-			{
-				digit = 1;
-
-			}
-			else
-			{
-				digit = 0;
-			}
-			stringstream ss;
-			ss << (tmp % 2);
-			res += ss.str();
-			end1--; end2--;
-		}
-
-		while (end1 >= 0)
-		{
-			tmp = a[end1] - '0' + digit;
-			if (tmp >= 2)
-			{
-				digit = 1;
-			}
-			else
-			{
-				digit = 0;
-			}
-			stringstream ss;
-			ss << (tmp % 2);
-			res += ss.str();
-			end1--;
-		}
-		while (end2 >= 0)
-		{
-			tmp = b[end2] - '0' + digit;
-			if (tmp >= 2)
-			{
-				digit = 1;
-
-			}
-			else
-			{
-				digit = 0;
-			}
-			stringstream ss;
-			ss << (tmp % 2);
-			res += ss.str();
-			end2--;
-		}
-		if (digit == 1)
-		{
-			stringstream ss;
-			ss << digit;
-			res += ss.str();
-		}
-
-		int first = 0, last = res.length() - 1;
-
-		while (first < last)
-		{
-			char tmp = res[first];
-			res[first] = res[last];
-			res[last] = tmp;
-			first++; last--;
-		}
-		return res;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res;
+		 int tmp, carry = 0;
+		 int lena = a.length(), lenb = b.length();
+		 while (lena || lenb) {
+			 int vala = lena ? a[lena - 1] - '0' : 0;
+			 int valb = lenb ? b[lenb - 1] - '0' : 0;
+			 tmp = vala + valb + carry;
+			 res = to_string(tmp % 2) + res;
+			 carry = tmp >= 2 ? 1 : 0;
+			 lena = lena > 0 ? lena - 1 : 0;
+			 lenb = lenb > 0 ? lenb - 1 : 0;
+		 }
+		 if (carry) res = "1" + res;
+		 return res;
+    }
+};
