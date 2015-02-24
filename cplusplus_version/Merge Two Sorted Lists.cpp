60d9700d2736fd18fe98ce3b6ffb45b71343f59e
wjk20120522 <jkwangbest@gmail.com>
2015-02-24 16:27:25 +0800

    same idea but shorter code

--- cplusplus_version/Merge Two Sorted Lists.cpp
+++ cplusplus_version/Merge Two Sorted Lists.cpp
@@ -42,3 +42,27 @@ public:
         
     }
 };
+
+
+/*
+compare tow listnode together to make code shorter
+ */
+class Solution {
+public:
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+		ListNode *res = new ListNode(-1);
+		ListNode *tmp = res;
+		while (l1 || l2) {
+			if (l1 && l2 && l1->val <= l2->val || l2 == NULL) {
+				tmp->next = l1;
+				l1 = l1->next;
+			}
+			else if (l1 && l2 && l1->val > l2->val || l1 == NULL) {
+				tmp->next = l2;
+				l2 = l2->next;
+			}
+			tmp = tmp->next;
+		}
+		return res->next;
+	}
+};
