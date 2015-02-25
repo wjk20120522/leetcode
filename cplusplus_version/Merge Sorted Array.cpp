e9402e3f791b9cce22e00b685b8186f1096c1d1f
wjk20120522 <jkwangbest@gmail.com>
2015-02-25 13:18:23 +0800

    short code and concrete clear code

--- cplusplus_version/Merge Sorted Array.cpp
+++ cplusplus_version/Merge Sorted Array.cpp
@@ -1,47 +1,32 @@
+/*
+concrete method
+ */
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int index = m + n - 1;
+        while (m || n) {
+            if (m && n && A[m - 1] >= B[n - 1] || n==0) {
+                A[index--] = A[m-1];
+                m = m ? m - 1 : 0;
+            }
+            else {
+                A[index--] = B[n-1];
+                n = n ? n - 1 : 0;
+            }   
+        }
+    }
+};
+
+
+/*
+use STL, sort of cheating
+ */
 class Solution {
 public:
     void merge(int A[], int m, int B[], int n) {
     	// use STL function
     	copy( B, B + n, A + m);
 		sort(A, A + m + n);
-		/** another concrete method
-		 *********
-        //put the num from the tail of array A
-        int index = m+n-1;
-        for(; index>=0; index--) {
-        	if(n<=0) {
-        		return ;
-        	} else if(m<=0) {
-        		A[index] = B[--n];
-        	} else if(A[m-1] < B[n-1]) {
-        		A[index] = B[--n];
-        	} else {
-        		A[index] = A[--m];
-        	}
-
-        }
-        ***********
-        **/
-        /*
-        int posA = m-1;
-        int posB = n-1;
-        int cur = m+n-1;
-        while(posA >=0 && posB >=0)
-        {
-            if(A[posA] > B[posB])
-            {
-                A[cur--] = A[posA--];
-            }
-            else
-            {
-                A[cur--] = B[posB--];
-            }
-        }
-        
-        while( posB >=0 )
-        {
-            A[cur--] = B[posB--];
-        }
-        */
     }
 };
