7daf3766a676842f39c695f9760d12fb981ea68c
wjk20120522 <jkwangbest@gmail.com>
2015-02-25 12:51:43 +0800

    shorter code

--- cplusplus_version/Remove Duplicates from Sorted List.cpp
+++ cplusplus_version/Remove Duplicates from Sorted List.cpp
@@ -9,26 +9,13 @@
 class Solution {
 public:
     ListNode *deleteDuplicates(ListNode *head) {
-        if (head == NULL || head->next == NULL) return head;
-		ListNode *cur = head;
-		ListNode *nex = head->next;
-
-		while (cur != NULL && cur->next != NULL)
-		{
-			nex = cur->next;
-			if (nex->val == cur->val )
-			{
-				//需要注意末尾重复的情况
-				while ( nex != NULL && nex->val == cur->val)
-				{
-					nex = nex->next;
-				}
-				cur->next = nex;
-				cur = nex;
+        ListNode *cur = head;
+		while (cur && cur->next) {
+			if (cur->val == cur->next->val) {
+				cur->next = cur->next->next;
 			}
-			else
-			{
-				cur = cur->next;				
+			else {
+				cur = cur->next;
 			}
 		}
 		return head;
