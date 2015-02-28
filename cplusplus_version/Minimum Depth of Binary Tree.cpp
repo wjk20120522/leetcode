e2efdb11585ca6feb254dd1fff104558ccbd6009
wjk20120522 <jkwangbest@gmail.com>
2015-02-28 10:58:37 +0800

    simple recursive method

--- cplusplus_version/Minimum Depth of Binary Tree.cpp
+++ cplusplus_version/Minimum Depth of Binary Tree.cpp
@@ -4,7 +4,7 @@ public:
 		if (root == NULL) return 0;
 		int leftDepth = minDepth(root->left);
 		int rightDepth = minDepth(root->right);
-		//note: one of its child is NULL
+		//if one of its child is NULL, we should not return min of two
 		if (leftDepth*rightDepth == 0) return 1 + max(leftDepth, rightDepth);
 		return 1 + min(leftDepth, rightDepth);
 	}
