812e10baae070d2443565e46d66896bfbb788523
wjk20120522 <jkwangbest@gmail.com>
2015-02-28 16:37:04 +0800

    short code

--- cplusplus_version/Valid Palindrome.cpp
+++ cplusplus_version/Valid Palindrome.cpp
@@ -2,19 +2,16 @@
  * more neat code
  * 
  */
-
 class Solution {
 public:
-  bool isPalindrome(string s) {
-	  if (s.empty()) return true;
-	  int begin = 0, end = s.size() - 1;
-	  while (begin < end) {
-		  while (begin < end && !isalnum(s[begin])) begin++;
-		  while (begin < end && !isalnum(s[end])) end--;
-		  if (tolower(s[begin]) != tolower(s[end])) 
-			  return false;
-		  begin++; end--;
-	  }
-	  return true;
-  }
+	bool isPalindrome(string s) {
+		int begin = 0, end = s.length() - 1;
+		while (begin < end) {
+			while (begin < end && !isalnum(s[begin])) begin++;
+			while (begin < end && !isalnum(s[end])) end--;
+			if (tolower(s[begin]) != tolower(s[end])) return false;
+			begin++; end--;
+		}
+		return true;
+	}
 };
