9008b26e589e0d9299c031064ba9572bb7e93458
wjk20120522 <jkwangbest@gmail.com>
2015-02-28 11:48:10 +0800

    add some comment

--- cplusplus_version/Path Sum.cpp
+++ cplusplus_version/Path Sum.cpp
@@ -1,12 +1,3 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
     bool hasPathSum(TreeNode *root, int sum) {
@@ -20,52 +11,57 @@ public:
 };
 
 /*  iterative method, postorder traversal
-	bool hasPathSum(TreeNode *root, int sum) {
-        stack<TreeNode *> s;
-        TreeNode *pre = NULL, *cur = root;
-        int SUM = 0;
-        while (cur || !s.empty()) {
-            while (cur) {
-                s.push(cur);
-                SUM += cur->val;
-                cur = cur->left;
-            }
-            cur = s.top();
-            if (cur->left == NULL && cur->right == NULL && SUM == sum) {
-                return true;
-            }
-            if (cur->right && pre != cur->right) {
-                cur = cur->right;
-            } else {
-                pre = cur;
-                s.pop();
-                SUM -= cur->val;
-                cur = NULL;
-            }
-        }
-        return false;
-    }
  */
+bool hasPathSum(TreeNode *root, int sum) {
+    stack<TreeNode *> s;
+    TreeNode *pre = NULL, *cur = root;
+    int SUM = 0;
+    while (cur || !s.empty()) {
+        // find all left child
+        while (cur) {
+            s.push(cur);
+            SUM += cur->val;
+            cur = cur->left;
+        }
+        cur = s.top();
+        // find the leaf node
+        if (cur->left == NULL && cur->right == NULL && SUM == sum) {
+            return true;
+        }
+        // if has right child and not visit, then visit
+        if (cur->right && pre != cur->right) {
+            cur = cur->right;
+        } else {
+            // pop current node
+            pre = cur;
+            s.pop();
+            SUM -= cur->val;
+            cur = NULL;
+        }
+    }
+    return false;
+}
+
 
 /*  iterative method, preorder traversal
-	bool hasPathSum(TreeNode *root, int sum) {
-        stack<TreeNode*> st; stack<int> stval;
-        TreeNode *node{root};
-        int val = 0;
-        while (!st.empty() || node != NULL) {
-            if (node != NULL) {
-                val += node->val;
-                st.push(node);
-                stval.push(val);
-                node = node->left;
-            } else {
-                node = st.top(); st.pop();
-                val = stval.top(); stval.pop();
-                if (val == sum && node->left == NULL && node->right == NULL)
-                    return true;
-                node = node->right;
-            }
+*/
+bool hasPathSum(TreeNode *root, int sum) {
+    stack<TreeNode*> st; stack<int> stval;
+    TreeNode *node{root};
+    int val = 0;
+    while (!st.empty() || node != NULL) {
+        if (node != NULL) {
+            val += node->val;
+            st.push(node);
+            stval.push(val);
+            node = node->left;
+        } else {
+            node = st.top(); st.pop();
+            val = stval.top(); stval.pop();
+            if (val == sum && node->left == NULL && node->right == NULL)
+                return true;
+            node = node->right;
         }
-        return false;
-	}
- */
+    }
+    return false;
+}
