a157016485f126d4f3b01499d7dc89bf41e242cb
wjk20120522 <jkwangbest@gmail.com>
2015-03-01 19:40:30 +0800

    make it shorter..

--- cplusplus_version/Same Tree.cpp
+++ cplusplus_version/Same Tree.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
 	bool isSameTree(TreeNode *p, TreeNode *q) {
-		if (p == NULL && q == NULL) return true;
-		if (p == NULL || q == NULL) return false;
-		if (p->val == q->val)
-			return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-		return false;
+		if (p == NULL || q == NULL) return p==q;
+		return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 	}
 };
