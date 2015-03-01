aa175ed500bf6d50ed9a471613a9be6a3feb3095
wjk20120522 <jkwangbest@gmail.com>
2015-03-01 09:56:10 +0800

    clear code

--- cplusplus_version/ZigZag Conversion.cpp
+++ cplusplus_version/ZigZag Conversion.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     string convert(string s, int nRows) {
-        if (s.length() == 0 || nRows <= 0) return "";
-		if (nRows == 1) return s;
-		string res = s;
-		int nums = 2 * nRows - 2;
-		int index = 0;
-		for (int i = 0; i<nRows; i++)
-		{
-			for (int j = i; j<s.length(); j += nums)
-			{
-				res[index++] = s[j];
-				if (i != 0 && i != nRows-1 && j + nums - 2 * i<s.length())
-				{
-					res[index++] = s[j + nums - 2 * i];
+        if (nRows == 1) return s;
+		int len = s.length(), gap = nRows * 2 - 2;
+		string res;
+		for (int i = 0; i < nRows; i++) {
+			for (int j = i; j < len; j += gap) {
+				res += s[j];
+				// if not the first or last row
+				if (i!=0 && i!= nRows-1 && j + 2 * (nRows - i - 1) < len) {
+					res += s[j + 2 * (nRows - i - 1)];
 				}
 			}
 		}
