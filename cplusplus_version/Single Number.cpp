2175d738889501557c2d210d2fbdd500d505d7cb
wjk20120522 <jkwangbest@gmail.com>
2015-03-02 22:28:44 +0800

    add a smart method by others short code

--- cplusplus_version/Single Number.cpp
+++ cplusplus_version/Single Number.cpp
@@ -30,3 +30,16 @@ public:
         return res;
     }
 };
+
+
+/*
+hard to understand but smart
+ */
+public int singleNumber(int[] A, int n) {
+    int ones = 0, twos = 0;
+    for(int i = 0; i < n; i++){
+        ones = (ones ^ A[i]) & ~twos;
+        twos = (twos ^ A[i]) & ~ones;
+    }
+    return ones;
+}
