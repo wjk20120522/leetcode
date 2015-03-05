e64a35697e2062e28204d5e17adfaf962d4e21a9
wjk20120522 <jkwangbest@gmail.com>
2015-03-05 22:37:43 +0800

    add a DFS method and a short code

--- cplusplus_version/Sum Root to Leaf Numbers.cpp
+++ cplusplus_version/Sum Root to Leaf Numbers.cpp
@@ -1,3 +1,51 @@
+/*
+DFS, easy to understand
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode *root) {
+		return DFS(root, 0);
+	}
+
+	int DFS(TreeNode*root, int sum) {
+		if (root == NULL) return 0;
+
+		//current value of node
+		sum = sum * 10 + root->val;
+
+		int leftVal = DFS(root->left, sum);
+		int rightVal = DFS(root->right, sum);
+		if (leftVal + rightVal == 0) {	// current node is leaf
+			return sum;
+		}
+		return leftVal + rightVal;
+	}
+};
+
+
+/*
+make the above code shorter
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode *root) {
+		return DFS(root, 0);
+	}
+
+	int DFS(TreeNode*root, int sum) {
+		if (root == NULL) return 0;
+		//current value of node
+		sum = sum * 10 + root->val;
+		
+		int curVal = DFS(root->left, sum) + DFS(root->right, sum);
+		return curVal == 0 ? sum : curVal;
+	}
+};
+
+
+/*
+BFS, change the value of tree
+ */
 class Solution {
 public:
     int sumNumbers(TreeNode *root) {
