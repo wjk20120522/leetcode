64b7404badc729d147ea6e2466e0b026e244239b
wjk20120522 <jkwangbest@gmail.com>
2015-03-07 15:13:44 +0800

    fast code and clean code

--- cplusplus_version/Best Time to Buy and Sell Stock.cpp
+++ cplusplus_version/Best Time to Buy and Sell Stock.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        if (prices.size() < 2) return 0;
-		int profit = 0;
-		int min, max;
-		
-		min = max = prices[0];
-
-		for (int i=0; i<prices.size(); i++)
-		{
-			if (prices[i] - min > profit)
-			{
-				profit = prices[i] - min;
-			}
-			if (prices[i] < min)
-			{
-				min = prices[i];
-			}
+    	int maxprofit = 0, minprice = INT_MAX;
+		for (int i = 0; i < prices.size(); i++) {
+			minprice = min(prices[i], minprice);
+			maxprofit = max(prices[i] - minprice, maxprofit);	
 		}
-		return profit;
+		return maxprofit;
     }
 };
