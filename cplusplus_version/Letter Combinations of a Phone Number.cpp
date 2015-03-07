755a75647d7b7247227253153ee92ca1ea7aa332
wjk20120522 <jkwangbest@gmail.com>
2015-03-07 11:28:27 +0800

    iterator digit one by one

--- cplusplus_version/Letter Combinations of a Phone Number.cpp
+++ cplusplus_version/Letter Combinations of a Phone Number.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-	vector<string> letterCombinations(string digits) {
-		string alph [][4] = { {}, {}, { "a", "b", "c" }, { "d", "e", "f" }, { "g", "h", "i" },
-		{ "j", "k", "l" }, { "m", "n", "o" }, { "p", "q", "r", "s" }, { "t", "u", "v" }, {"w","x","y","z"} };
+    vector<string> letterCombinations(string digits) {
+		string alphs[] = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 		vector<string>res,tmp;
+		if (digits == "") return res;
+		res.push_back("");
 		for (int i = 0; i < digits.length(); i++) {
 			int num = digits[i] - '0';
-			
-			if (i == 0) { // first letter
-				tmp.push_back(alph[num][0]);
-				tmp.push_back(alph[num][1]);
-				tmp.push_back(alph[num][2]);
-			}
-			else {
-				for (string str : res) {
-					tmp.push_back(str + alph[num][0]);
-					tmp.push_back(str + alph[num][1]);
-					tmp.push_back(str + alph[num][2]);
-				}
-			}
-			if (num == 7 || num == 9) {
-				if (i == 0) tmp.push_back(alph[num][3]);
-				else {
-					for (string str : res) {
-						tmp.push_back(str + alph[num][3]);
-					}
+			for (string str : res) {
+				for (int j = 0; j < alphs[num].length(); j++) {
+					tmp.push_back(str + alphs[num][j]);
 				}
 			}
 			res = tmp;
