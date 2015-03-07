f0630e9ef225a6bfccf3cd6596a6cbd257393592
wjk20120522 <jkwangbest@gmail.com>
2015-03-07 17:46:36 +0800

    add a short code

--- cplusplus_version/Best Time to Buy and Sell Stock II.cpp
+++ cplusplus_version/Best Time to Buy and Sell Stock II.cpp
@@ -12,3 +12,18 @@ public:
 		return maxprofit;
     }
 };
+
+
+/*
+short code
+ */
+class Solution {
+public:
+	int maxProfit(vector<int> &prices) {
+		int maxprofit = 0;
+		for (int i = 1; i < prices.size(); i++) {
+			maxprofit += (prices[i]> prices[i - 1] ? prices[i] - prices[i - 1] : 0);
+		}
+		return maxprofit;
+	}
+};
