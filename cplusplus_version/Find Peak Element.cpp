42c20c5c3e98ce228514273eff9d1ff521e48e7f
wjk20120522 <jkwangbest@gmail.com>
2015-03-07 11:47:03 +0800

    iterative method

--- cplusplus_version/Find Peak Element.cpp
+++ cplusplus_version/Find Peak Element.cpp
@@ -28,3 +28,34 @@ public:
 	 }
 	 
 };
+
+
+/*
+iterative method
+ */
+class Solution {
+public:
+	int findPeakElement(const vector<int> &num) {
+		int len = num.size();
+		int start = 0, end = len - 1, mid = (start + end) / 2;
+		if (len == 1) return 0;
+		if (len == 2) return num[0] > num[1] ? 0 : 1;
+		while (true)
+		{
+			if (mid == 0) {
+				return num[0] > num[1] ? 0 : 1;
+			}
+			if (mid == len - 1) {
+				return num[len - 1] > num[len - 2] ? len - 1 : len - 2;
+			}
+			if (num[mid] > num[mid - 1] && num[mid] > num[mid + 1]) return mid;
+			else if (num[mid] < num[mid - 1] && num[mid + 1] < num[mid]) {
+				end = mid - 1;
+			}
+			else {
+				start = mid + 1;
+			}
+			mid = (start + end) / 2;
+		}
+	}
+};
