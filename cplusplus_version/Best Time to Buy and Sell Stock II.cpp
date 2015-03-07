20be0bd9a1e99c858b1409383bea616a1e5565f5
wjk20120522 <jkwangbest@gmail.com>
2015-03-07 15:31:01 +0800

    short code

--- cplusplus_version/Best Time to Buy and Sell Stock II.cpp
+++ cplusplus_version/Best Time to Buy and Sell Stock II.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        int profit = 0;
-		int sum_profit = 0;
-		int min, max;
-		if (prices.size() == 0) return 0;
-
-		min = max = prices[0];
-
-		for (int i=0; i<prices.size(); i++)
-		{
-			if (prices[i] - min > profit)
-			{
-				profit = prices[i] - min;
-				max = prices[i];
-			}
-			
-			if (prices[i] < max)
-			{
-				sum_profit += profit;
-				profit = 0;
-				max = min = prices[i];
+        int maxprofit = 0, minprice = INT_MAX;
+		for (int i = 0; i < prices.size(); i++) {
+			minprice = min(minprice, prices[i]);
+			if (prices[i] > minprice) {	// get that profit if you can
+				maxprofit += prices[i] - minprice;
+				minprice = prices[i];
 			}
 		}
-		//此处需要考虑结束时最后一个profit没有加到sum_profit里面。
-		// 比如 [1, 5, 2, 4, 6]
-		return sum_profit+profit;
+		return maxprofit;
     }
 };
