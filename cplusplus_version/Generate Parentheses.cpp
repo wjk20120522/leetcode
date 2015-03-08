c28ddb860124028e5b36f9ecb3eb0e7f8e48a6ce
wjk20120522 <jkwangbest@gmail.com>
2015-03-08 09:10:48 +0800

    add a short code

--- cplusplus_version/Generate Parentheses.cpp
+++ cplusplus_version/Generate Parentheses.cpp
@@ -18,3 +18,21 @@ public:
 		}
 	}
 };
+
+
+/*
+short but cost more time
+ */
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        if(n==0) return vector<string>(1,"") ;
+        if(n==1) return vector<string>(1,"()") ;
+        vector<string> result;
+        for(int i=0;i!=n;i++)
+            for(auto inner: generateParenthesis(i))
+                for(auto outter:  generateParenthesis(n-i-1))
+                    result.push_back("("+inner+")"+outter);
+        return result;
+    }
+};
