310efa50c44f16be24e338da274f54ca1efe0644
wjk20120522 <jkwangbest@gmail.com>
2015-03-08 10:52:32 +0800

    add a shorter code for II

--- cplusplus_version/Populating Next Right Pointers in Each Node II.cpp
+++ cplusplus_version/Populating Next Right Pointers in Each Node II.cpp
@@ -33,3 +33,29 @@ public:
 		}
 	}
 };
+
+
+/*
+same idea, but short
+ */
+class Solution {
+public:
+	void connect(TreeLinkNode *root) {
+		TreeLinkNode *cur = root, *child, *pre;
+		while (cur) {
+			child = pre = NULL;
+			while (cur) {
+				if (cur->left) {
+					if (pre) pre = pre->next = cur->left;
+					else pre = child = cur->left;
+				}
+				if (cur->right) {
+					if (pre) pre = pre->next = cur->right;
+					else pre = child = cur->right;
+				}
+				cur = cur->next;
+			}
+			cur = child;
+		}
+	}
+};
--- cplusplus_version/Populating Next Right Pointers in Each Node.cpp
+++ cplusplus_version/Populating Next Right Pointers in Each Node.cpp
@@ -27,31 +27,17 @@ II's code can be used here,two
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-        TreeLinkNode *cur = root, *child, *tmp;
-        bool firstchild;
+        TreeLinkNode *cur = root, *child, *pre;
         while (cur) {
-            firstchild = false;
-            child = cur->left;
+            child = pre = NULL;
             while (cur) {
                 if (cur->left) {
-                    if (firstchild) {
-                        tmp->next = cur->left;
-                        tmp = cur->left;
-                    }
-                    else {
-                        firstchild = true;
-                        tmp = child = cur->left;
-                    }
+                    if (pre) pre = pre->next = cur->left;
+                    else pre = child = cur->left;
                 }
                 if (cur->right) {
-                    if (firstchild) {
-                        tmp->next = cur->right;
-                        tmp = cur->right;
-                    }
-                    else {
-                        firstchild = true;
-                        tmp = child = cur->right;
-                    }
+                    if (pre) pre = pre->next = cur->right;
+                    else pre = child = cur->right;
                 }
                 cur = cur->next;
             }
