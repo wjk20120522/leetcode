79e32a238e371cfa6c44c8bfa3d78afa9c9c43e6
wjk20120522 <jkwangbest@gmail.com>
2015-03-09 18:05:00 +0800

    similar with above method

--- cplusplus_version/Rotate Image.cpp
+++ cplusplus_version/Rotate Image.cpp
@@ -31,10 +31,34 @@ public:
 				swap(matrix[i][j], matrix[n - 1 - i][j]);
 			}
 		}
-		// exchange left up with right down
+		// exchange left down with right up
 		for (int i = 0; i < n; i++) {
 			for (int j = i + 1; j < n; j++) {
 				swap(matrix[i][j], matrix[j][i]);
+			}
+		}
+    }
+};
+
+
+/*
+similar with above
+ */
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+		if (n <= 1) return;
+		// exchange half left with half right
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n/2; j++) {
+				swap(matrix[i][j], matrix[i][n-1-j]);
+			}
+		}
+		// exchange left up with right down
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n-1-i; j++) {
+				swap(matrix[i][j], matrix[n-1-j][n-1-i]);
 			}
 		}
     }
