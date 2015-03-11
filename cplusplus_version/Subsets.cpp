2ee285b9a406e765d8999a6324489d79af033faa
Jiankang Wang <jkwangbest@gmail.com>
2015-03-11 18:34:04 +0800

    add a method of DFS

--- cplusplus_version/Subsets.cpp
+++ cplusplus_version/Subsets.cpp
@@ -7,8 +7,8 @@ public:
 
 		 DFS(S, 0, res, sub);
 		 return res; 
-
 	 }
+
 	 void DFS(vector<int> &s, int begin, vector< vector<int> > &res, vector<int> &sub) {
 		 res.push_back(sub);
 		 
@@ -18,4 +18,30 @@ public:
 			 sub.pop_back();
 		 }
 	 }
+};
+
+
+/*
+similar method
+*/
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        		vector<vector<int> > res;
+        		sort(S.begin(), S.end());
+        		vector<int>tmp;
+        		getsubsets(res, 0, tmp, S);
+        		return res;
+    	}
+
+    	void  getsubsets(vector<vector<int> >&res, int idx, vector<int>&tmp, vector<int>&S) {
+    		if(idx == S.size()) {
+    			res.push_back(tmp);
+    			return ;
+    		}
+    		tmp.push_back(S[idx]);
+    		getsubsets(res, idx+1, tmp, S);
+    		tmp.pop_back();
+    		getsubsets(res, idx+1, tmp, S);
+    	}
 };
