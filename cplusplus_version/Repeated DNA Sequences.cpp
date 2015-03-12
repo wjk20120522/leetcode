8c11d916ab5f310f7a65ee9d924bd76ba9f1b4ca
wjk20120522 <jkwangbest@gmail.com>
2015-03-12 21:10:32 +0800

    more quickly, same idea

--- cplusplus_version/Repeated DNA Sequences.cpp
+++ cplusplus_version/Repeated DNA Sequences.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-	vector<string> findRepeatedDnaSequences(string s) {
-		int len = s.length();
-		int times = len - 9;
-
-		map<int,int>mp;
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<int, int>mp;
 		vector<string> res;
-		string str;
-		for (int i = 0; i < times; i++) {
-			str = s.substr(i, 10);
-			int tmp = 0;
-			for (int j = 0; j < 10; j++) {
-				tmp = (tmp << 3 | str[j] & 7);
+		int val, times;
+		for (int i = 0; i <= (int)s.length() - 10; i++) {
+			val = 0;
+			for (int j = i; j < i + 10; j++) {
+				val |= (s[j] & 7) << (j - i)*3;
 			}
-			if (mp.find(tmp) != mp.end()) {
-				if (mp[tmp] == 1) {
-					res.push_back(str);
-					mp[tmp] = 2;
-				}
+			if (mp.find(val) != mp.end()) {
+				if (mp[val] == 1)
+					res.push_back(s.substr(i, 10));
+				mp[val]++;
 			}
 			else {
-				mp.insert(make_pair(tmp, 1));
+				mp[val] = 1;
 			}
 		}
 		return res;
-
-
-	}
+    }
 };
