1979011d03e41d30a107fe92690ecf47f5a77c77
wjk20120522 <jkwangbest@gmail.com>
2015-03-15 10:25:33 +0800

    more readable

--- cplusplus_version/Letter Combinations of a Phone Number.cpp
+++ cplusplus_version/Letter Combinations of a Phone Number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-		string alphs[] = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-		vector<string>res,tmp;
-		if (digits == "") return res;
+		string digit2str[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+		vector<string> res, tmp;
+		if (digits.length() == 0) return res;
 		res.push_back("");
 		for (int i = 0; i < digits.length(); i++) {
+			tmp.clear();
 			int num = digits[i] - '0';
-			for (string str : res) {
-				for (int j = 0; j < alphs[num].length(); j++) {
-					tmp.push_back(str + alphs[num][j]);
+			for (int j = 0; j < digit2str[num].length(); j++) {
+				for (string str : res) {
+					tmp.push_back(str + (digit2str[num][j]));
 				}
 			}
 			res = tmp;
-			tmp.clear();
 		}
 		return res;
 	}
