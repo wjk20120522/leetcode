595df9c3fc440443eb694117533ed9f61e3d3040
wjk20120522 <jkwangbest@gmail.com>
2015-03-23 22:57:18 +0800

    same idea but make it shorter

--- cplusplus_version/Fraction to Recurring Decimal.cpp
+++ cplusplus_version/Fraction to Recurring Decimal.cpp
@@ -3,34 +3,23 @@ public:
     string fractionToDecimal(int numerator, int denominator) {
         string res;
 		if (numerator == 0) return "0";
-		//note the negative
-		if (numerator < 0 ^ denominator < 0) res += "-";
-		
-		// note here may occure a big mistake , just use int64_t n = (abs)numerator
-		int64_t n = numerator, d = denominator;
-		n = abs(n);
-		d = abs(d);
-
-		// the interger part
-		res += to_string(n / d);
-		// no fraction
-		if (n % d == 0) return res;
+		if (numerator > 0 ^ denominator > 0) res += "-";
+		long long n = numerator, d = denominator;
+		n = abs(n), d = abs(d);
+		res += to_string(n/d);
+		if (n%d==0) return res;
 		res += ".";
-		int64_t r;
+		unordered_map<int, int> mp;
 
-		//calculate fraction
-		unordered_map<int, int> map;
-		for (r = n%d; r; r = r%d) {
-			if (map.find(r) != map.end()) { // recurring occures
-				res.insert(map[r], 1, '(');
-				res += ')';
-				return res;
+		for (long long int r = n%d; r; r = r%d) {
+			if (mp.find(r) != mp.end()) {  // occures again
+				res.insert(mp[r], 1, '(');
+				res += ")"; return res;
 			}
-			map[r] = res.size();
+			mp[r] = res.size();
 
 			r *= 10;
 			res += to_string(r / d);
-			
 		}
 		return res;
     }
