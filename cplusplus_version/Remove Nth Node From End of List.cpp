543945253b8d387c4d4d3eaf01ce9246fd8b3697
wjk20120522 <jkwangbest@gmail.com>
2015-09-18 21:07:28 +0800

    short code

--- cplusplus_version/Remove Nth Node From End of List.cpp
+++ cplusplus_version/Remove Nth Node From End of List.cpp
@@ -1,54 +1,16 @@
-/*
-two pass
- */
 class Solution {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
-        if(head == NULL) return NULL;
-        int nums = get_nums(head);
-        if(nums == n) {
-            return head->next;
+        ListNode *quick = head, *slow = head;
+        for(int i=0; i<n; i++) {
+            quick = quick->next;
         }
-
-        ListNode *pre = head;
-        for(int i=0; i<nums-n-1; i++) {
-            pre = pre->next;
+        if(quick == NULL) return head->next;  //remove the head;
+        while(quick->next) {
+            quick = quick->next;
+            slow = slow->next;
         }
-    
-        pre->next = pre->next->next;
-        return head;
-    }
-    
-    int get_nums(ListNode *head) {
-        int count = 0;
-        while(head != NULL) {
-            count ++;
-            head = head->next;
-        }
-        return count;
-    }
-};
-
-
-/*
-one pass
- */
-class Solution {
-public:
-    ListNode *removeNthFromEnd(ListNode *head, int n) {
-        ListNode *first = head, *second = head, *pre = first;
-        for (int i = 0; i < n; i++) {
-            second = second->next;
-        }
-        if (second == NULL) {
-            return first->next;
-        }
-        while (second) {            
-            pre = first;
-            first = first->next;
-            second = second->next;
-        }
-        pre->next = first->next;
+        slow->next = slow->next->next;
         return head;
     }
 };
